6c0ff177056c97af5b627beb07be472c7105a948
dew <salman.giantssoft@gmail.com>
2023-09-06 11:25:25 +0600

    Assignment On Module - 14

--- src/app/api/dashboard/route.js
+++ src/app/api/dashboard/route.js
@@ -3,7 +3,10 @@ import { NextResponse } from "next/server";
 
 export async function GET(request, response) {
   const token = await request.cookies.get("rt");
-  const payload = await VerifyToken(token["value"]);
+  const payload = "";
+  if (token) {
+    payload = await VerifyToken(token["value"]);
+  }
   if (payload) {
     return NextResponse.json({ status: true, userInfo: payload });
   } else {
